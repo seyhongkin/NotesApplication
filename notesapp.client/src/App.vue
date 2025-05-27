3aa4a212405a40102510ba3b9739f4fd934d1384
seyhongkin <kin19seyhong@gmail.com>
2025-05-27 14:10:40 +0700

    first commit

---
+++ notesapp.client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import Book from './assets/icons/Book.vue'
+</script>
+
+<template>
+  <div class="max-w-4xl mx-auto px-4">
+
+    <header class="my-4">
+      <h1 class="font-bold text-xl">
+        <Book />
+        NoteApp
+      </h1>
+    </header>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
